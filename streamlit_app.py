a9ff20dfa2830452a76afee7baa41cfc46b5004b
rafalk <44731723+rafalkozaczuk@users.noreply.github.com>
2023-03-01 16:45:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ streamlit.header("The fruits from Snowflake:")
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
             my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-            return my_data_rows = my_cur.fetchall()
+            return  my_cur.fetchall()
 
 
 if streamlit.button('Get Fruit load'):
