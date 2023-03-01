fa6fab2bb7c42569c4a1ba8c4222d4fe615feea0
rafalk <44731723+rafalkozaczuk@users.noreply.github.com>
2023-03-01 15:14:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 #SNOWFLAKE
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUITYVICE")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
